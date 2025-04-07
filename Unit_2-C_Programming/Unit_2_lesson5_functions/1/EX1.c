697bb0596aae2406d2497e729b80b5fc172c878b
FatmaAshraf76 <143643641+FatmaAshraf76@users.noreply.github.com>
2025-04-07 04:57:23 +0200

    Update EX1.c

--- Unit_2-C_Programming/Unit_2_lesson5_functions/1/EX1.c
+++ Unit_2-C_Programming/Unit_2_lesson5_functions/1/EX1.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <math.h>
-void prime(int first, int last)
-{
+
+void prime(int first, int last) {
     int n, i, is_prime;
-
-    printf("Prime numbers between %d and %d are: ", first, last);
-
-    for (n=first; n<=last; n++)
-    {
-        if(n<2)
-        	continue;
-        is_prime=1;
-        for (i=2; i<=sqrt(n); i++)
-        {
-            if (n%i==0)
-            {
+    for (n = first; n <= last; n++) {
+        if (n < 2) continue;
+        is_prime = 1;
+        for (i = 2; i <= sqrt(n); i++) {
+            if (n % i == 0) {
                 is_prime = 0;
                 break;
             }
         }
-        if (is_prime) // إذا كان العدد أوليًا، نطبعه
+        if (is_prime)
             printf("%d ", n);
     }
 }
@@ -29,5 +22,12 @@ int main(void)
 	printf("Enter two numbers (intervals): ");
 	fflush(stdout);
 	scanf("%d %d",&x,&y);
+	if (x==1)
+	{
+	    printf("1 ");
+	    x=2;
+	}
 	prime(x,y);
+    printf("\n");
+    return 0;
 }
